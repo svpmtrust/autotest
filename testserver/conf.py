364e7c44dda9b7319b01de56a360b2d79f62f6c1
siddharth <siddharthg.g7@gmail.com>
2014-01-10 11:36:41 +0530

    modified c and python

--- testserver/conf.py
+++ testserver/conf.py
@@ -1,6 +1,6 @@
 participant_dir = '/home/svpmtrust/participants/'
 program_dir='/home/svpmtrust/Programs/'
 mail_dir='/home/svpmtrust/mails/'
-git_host= '192.168.1.107:8080'
+git_host= '192.168.1.105:8080'
 
 
