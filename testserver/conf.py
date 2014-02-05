f3def614f15d5a20682f0a5990b23e827a8fbb07
gotpredictions <eswar.vandanapu@gnana.com>
2014-02-05 14:09:11 +0530

    Updated template and reading the configuration from environment

--- testserver/conf.py
+++ testserver/conf.py
@@ -1,6 +1,6 @@
-participant_dir = '/home/svpmtrust/participants/'
-program_dir='/home/svpmtrust/Programs/'
-mail_dir='/home/svpmtrust/mails/'
-git_host= '192.168.1.105:8080'
+import os
 
-
+participant_dir = os.environ.get('PARTICIPANT_DIR', '/home/svpmtrust/participants/')
+program_dir = os.environ.get('PROGRAM_DIR', '/home/svpmtrust/Programs/')
+mail_dir = os.environ.get('MAIL_DIR', '/home/svpmtrust/mails/')
+git_host = os.environ.get('GIT_HOST', '192.168.1.105:8080')
