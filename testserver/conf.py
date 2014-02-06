84cfe76c6b91edfc070707c35aeb3316e5894e15
gotpredictions <eswar.vandanapu@gnana.com>
2014-02-06 21:18:49 +0530

    Additional fixes

--- testserver/conf.py
+++ testserver/conf.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-participant_dir = '/home/svpmtrust/participants/'
-program_dir='/home/svpmtrust/Programs/'
-mail_dir='/home/svpmtrust/mails/'
-git_host= '192.168.1.102:8080'
-=======
 import os
->>>>>>> branch 'master' of https://github.com/svpmtrust/autotest.git
 
 participant_dir = os.environ.get('PARTICIPANT_DIR', '/home/svpmtrust/participants/')
 program_dir = os.environ.get('PROGRAM_DIR', '/home/svpmtrust/Programs/')
