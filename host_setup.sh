d6a480ca14b1a0059fbf8198128fcff267f3a675
Eswar <eswar.vandanapu@gmail.com>
2015-01-08 15:47:11 +0530

    Minor changes

--- host_setup.sh
+++ host_setup.sh
@@ -8,3 +8,6 @@ mongodb=echo $mongodbsetting | sed -r 's/^.*"([^"]*)"[,\r\n ]*$/\1/'
 # TODO: Remove hardcoded autotest and use the sed above to find details
 mongo autotest testdata/contest.js
 mongo autotest testdata/problemrepository.js
+
+# Install the required vagrant box
+vagrant box add precise64 http://files.vagrantup.com/precise64.box
