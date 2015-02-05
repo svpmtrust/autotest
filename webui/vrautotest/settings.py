fc7cb155df4d6d792427a809b42138ecb783870c
Ubuntu <ubuntu@ip-172-31-22-3.ap-southeast-1.compute.internal>
2015-02-05 15:24:38 +0000

    Changes from the server

--- webui/vrautotest/settings.py
+++ webui/vrautotest/settings.py
@@ -92,6 +92,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-	os.path.join(BASE_DIR, "static"),
-	'/var/www/static/',
+	#os.path.join(BASE_DIR, "static"),
+	#'/var/www/static/',
 )
+STATIC_ROOT = os.path.join(BASE_DIR,'../../static-files/static')
+
