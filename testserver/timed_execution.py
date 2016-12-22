a3a907cc77fde9c816e712e43f49b7d6d4167089
padmasreepotta <padmasree.potta@aviso.com>
2016-12-22 16:22:35 +0530

    rollback unwanted changes

--- testserver/timed_execution.py
+++ testserver/timed_execution.py
@@ -3,6 +3,7 @@ import time
 
 def check_output_with_timeout(*args, **kwargs):
     timeout = kwargs.pop('timeout', 5)    
+    outputs = ""
     p = subprocess.Popen(*args, stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE, **kwargs)
     while timeout > 0:
@@ -10,15 +11,22 @@ def check_output_with_timeout(*args, **kwargs):
         if result is not None:
             outputs, errors = p.communicate()
             if result == 0:
+                print "------"
+                print errors
+                print outputs
+                print "------"
                 return outputs
             else:
-                raise Exception('Program did not finish successfully. \n\n'+"".join([outputs,errors]))
+                return "This program did not return zero error code: "+str(result)+"\n"+outputs+"\n"+errors
+                #raise Exception('Program did not finish successfully. \n\n'+"".join([outputs,errors]))
         
         time.sleep(0.250)
         timeout -= 0.250
 
     p.kill()
-    raise TimedoutException('Program did not finish in given time')
+    return "This program took longer than allowed time (%s)" % timeout + outputs
+    #raise TimedoutException('Program did not finish in given time')
 
 class TimedoutException(Exception):
     pass
+
